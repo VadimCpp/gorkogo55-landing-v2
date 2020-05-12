091610b2e8ba1d8e06c282d3124ce9cc8c385ab1
Vadim Cpp <vadim.cpp@gmail.com>
2020-05-12 23:05:01 +0200

    #1 update working hours for saturday and sunday

--- js/working-hours.js
+++ js/working-hours.js
@@ -5,10 +5,26 @@ document.addEventListener("DOMContentLoaded", function() {
   // Код отображает "Сейчас открыто",
   // если ТЦ открыт в текущий момент времени
   //
-  var element = document.getElementById("text-open");
+
+  //
+  // С 12 МАЯ 2020 Г. ИЗМЕНЕН РЕЖИМ РАБОТЫ ТОРГОВОГО ЦЕНТРА:
+  // ПОНЕДЕЛЬНИК – ПЯТНИЦА С 10-00 ДО 18-00
+  // СУББОТА, ВОСКРЕСЕНЬЕ — ВЫХОДНЫЕ ДНИ
+  //
+  let element = document.getElementById("text-open");
   if (element) {
-    var kaliningradHours = new Date().getUTCHours() + 2;
-    var isMallOpened = kaliningradHours >= 10 && kaliningradHours < 18;
+    const SUNDAY = 0;
+    const SATURDAY = 6;
+
+    let now = new Date();
+    let kaliningradHours = now.getUTCHours() + 2;
+
+    let isMallOpened = true;    
+    if (now.getDay() === SUNDAY || now.getDay() === SATURDAY) {
+      isMallOpened = false;
+    } else if (kaliningradHours < 10 || kaliningradHours >= 18) {
+      isMallOpened = false;
+    }
 
     if (isMallOpened) {
       element.classList.remove("d-none");
